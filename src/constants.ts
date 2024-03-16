e1bcac4726d410e708a6573b8f9cfca3c2fe0eae
kitesurfdaniel <kitesurfdaniel@gmail.com>
2024-03-16 17:48:57 +0100

    changed defaults metadata

--- src/constants.ts
+++ src/constants.ts
@@ -7,8 +7,8 @@ export const CUSTOMER_NOT_DEFINED_ID = 'CUSTOMER_NOT_DEFINED_ID';
 export const HEADER_AUTH_TOKEN_KEY = 'vendure-auth-token';
 export const IMAGE_RESOLUTIONS = [1000, 800, 600, 400];
 export const HOMEPAGE_IMAGE = '/homepage.webp';
-export const DEFAULT_METADATA_URL = 'https://qwik-storefront.vendure.io/';
-export const DEFAULT_METADATA_TITLE = 'Vendure Qwik Storefront';
+export const DEFAULT_METADATA_URL = 'https://all-for-kite.com/';
+export const DEFAULT_METADATA_TITLE = 'Gear for all you loved watersports';
 export const DEFAULT_METADATA_DESCRIPTION =
 	'A headless commerce storefront starter kit built with Vendure & Qwik';
 export const DEFAULT_METADATA_IMAGE = 'https://qwik-storefront.vendure.io/social-image.png';
