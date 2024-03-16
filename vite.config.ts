c74398acf1f9856751f86b705f7f5e73def57e17
kitesurfdaniel <kitesurfdaniel@gmail.com>
2024-03-16 15:58:32 +0100

    updated for builder.io

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { qwikCity } from '@builder.io/qwik-city/vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { defineConfig } from 'vite';
+import { builderDevTools } from '@builder.io/dev-tools/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(async () => {
 	return {
 		ssr: { target: 'webworker' },
-		plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+		plugins: [qwikCity(), qwikVite(), tsconfigPaths(), builderDevTools()],
 		preview: {
 			headers: {
 				'Cache-Control': 'public, max-age=600',
